11482736812020aabd5f30b64bdb9e2cb6291245
rhidalgocipfpbatoi <rhidalgo@cipfpbatoi.es>
2020-10-08 16:14:24 +0200

    Proyecto con Ejemplos vistos en clase de la Unidad 2 de programación

---
+++ src/EjemploHasNextBucle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class EjemploHasNextBucle {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // scanner.hasNextInt();
+        // Parar la ejecución del programa y pedir un dato
+        // al usuario si el buffer está vacío.
+        // Si no está vacío, pregunta por el tipo de dato
+        // que está almacenado en el buffer, en concreto si
+        // es un entero pero no saca lo almacenado en buffer.
+        // boolean hayUnEnteroEnBuffer = scanner.hasNextInt();
+
+        // scanner.nextInt();
+        // Parar la ejecución del programa y pedir un dato
+        // al usuario si el buffer está vacío.
+        // Si no está vacío, saca la información almacenada
+        // en buffer en forma de entero.
+        // int radio = scanner.nextInt();
+
+        // scanner.nextLine();
+        // Limpia el buffer
+
+    }
+
+}
