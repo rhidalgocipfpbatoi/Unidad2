d1cc11c22de631d177254d9f699e77f1f244056a
rhidalgocipfpbatoi <rhidalgo@cipfpbatoi.es>
2020-10-13 16:06:30 +0200

    Práctica 14 Unidad 2

---
+++ src/Practica14U2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Practica14U2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+
+        do {
+            System.out.println("Dime un número:");
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+
+                if (numero < 0 || numero > 10) {
+                    System.out.println("El número no está entre 0 y 10");
+                }
+            } else {
+                System.out.println("No has introducido un entero");
+                numero = -1;
+            }
+
+            scanner.nextLine();
+
+        } while(numero < 0 || numero > 10);
+    }
+}
