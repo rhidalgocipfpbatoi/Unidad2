1c4c0d4e170eaebba092803cf0098990eac127ee
rhidalgocipfpbatoi <rhidalgo@cipfpbatoi.es>
2020-10-15 15:45:29 +0200

    Ejemplo de dibujo de línea con for (con incremento y con decremento)

---
+++ src/EjemploFor.java
@@ -0,0 +1,22 @@
+/**
+ * Práctica 8 de la unidad 2
+ */
+
+public class EjemploFor {
+    private static final int LONGITUD_LINEA = 100;
+
+    public static void main(String[] args) {
+
+        for (int i = LONGITUD_LINEA; i>=0; i--) {
+
+            System.out.print("_");
+        }
+
+        for (int i = 0; i<LONGITUD_LINEA; i++) {
+
+            System.out.print("_");
+        }
+
+
+    }
+}
